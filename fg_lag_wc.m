9229c005a1808264c3802ae487bd11cc9eaf1ac5
duanzhang557 <44869636+duanzhang557@users.noreply.github.com>
2019-05-08 23:12:08 +0800

    Delete fg_lag_wc.m

--- fg_lag_wc.m
+++
@@ -1,9 +0,0 @@
-   %fg_lag_wc伯德图几何法设计滞后校正控制器
-   function [ngc,dgc]=fg_lag_wc(ng0,dg0,w,wc)
-   ngv=polyval(ng0,j*wc);
-   dgv=polyval(dg0,j*wc);
-   g=ngv/dgv;
-   alph=abs(1/g);
-   T=10/(alph*wc);
-   ngc=[alph*T,1];
-   dgc=[T,1];
